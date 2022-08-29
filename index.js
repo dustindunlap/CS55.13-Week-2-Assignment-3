a3d074e938c8cc6576a3b9d72d28c1d4f16e9cb7
dus <111546791+dustindunlap@users.noreply.github.com>
2022-08-29 23:27:30 +0000

    Added functionality for server response. Built core html page. Added JSON object file.

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const fs = require("fs").promises;
 // respond to http requests
 const requestlistener = function(req, res){
   console.log(req.url);
+
+  if (req.url === "/"){
+    //if root, return html
+    fs.readFile( __dirname + '/root.html' ).then(
+      contents => {
+        //set response header
+        res.setHeader("Content-Type", "text/html; charset=UTF-8");
+        //return 200 ok status code
+        res.writeHead(200);
+        //send back file contents + close response
+        res.end(contents);
+      }
+    );
+  }else{
+     // return json if request not root
+    fs.readFile( __dirname + '/objs.json').then(
+      contents => {
+        res.setHeader("Content-Type", "application/json; charset=UTF-8");
+        res.writeHead(200);
+        res.end(contents);
+      }
+    );
+  }
 }
 //create http server instance
 const server = http.createServer(requestlistener);
